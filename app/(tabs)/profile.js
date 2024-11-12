19270269b4d7717438773317dd4b05ebb6219409
naja moe <najamoe@outlook.dk>
2024-11-12 20:27:46 +0100

    added signout button

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { signOutUser } from "../firebase/auth";
+
+import CustomButton from "../components/CustomButton";
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.container}>
+          <CustomButton
+            title="Sign Out"
+            style={styles.signOutButton}
+            handlePress={signOutUser}
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
+  );
+};
 
-export default profile
+export default profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#cae9f5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  signOutButton: {
+    marginBottom: 10,
+  },
+});
