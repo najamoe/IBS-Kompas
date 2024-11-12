ebfd9702617ee4a8b0c231fd180ba3dbc2acc6ad
naja moe <najamoe@outlook.dk>
2024-11-12 14:12:46 +0100

    added screens

---
+++ app/(tabs)/insert.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const insert = () => {
+  return (
+    <View>
+      <Text>insert</Text>
+    </View>
+  )
+}
+
+export default insert
+
+const styles = StyleSheet.create({})
