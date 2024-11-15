3a2589be2da022f0679ed8c8a6fffc2739fd9b78
naja moe <najamoe@outlook.dk>
2024-11-15 13:44:28 +0100

    added sendResetPassword from firebase

--- app/firebase/auth.js
+++ app/firebase/auth.js
@@ -1,4 +1,4 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
 import Toast from 'react-native-toast-message';
 import { auth } from "./FirebaseConfig"; 
 import { router } from "expo-router";
@@ -49,4 +49,39 @@ export const signOutUser = () => {
     .catch((error) => {
       console.error("Sign out error:", error.message);
     });
+};
+
+export const resetPassword = (email) => {
+  if (!email) {
+      Toast.show({
+        type: 'error',
+        text1: 'nulstilling mislykkedes',
+        text2: 'Indtast en email for at nulstille adgangskoden',  
+        visibilityTime: 5000,
+        position: 'top',
+      });
+      return Promise.reject("Email is missing");
+  }
+
+  return sendPasswordResetEmail(auth, email)
+      .then(() => {
+          Toast.show({
+            type: 'success',
+            text1: 'Email til nulstilling af password afsendt',
+            text2: 'Tjek din email for yderligere instruktioner',  
+            visibilityTime: 5000,
+            position: 'top',
+          });
+      })
+      .catch((error) => {
+          console.error("Reset error:", error.message);
+          Toast.show({
+            type: 'error',
+            text1: 'Nulstilling mislykkedes',
+            text2: 'Kunne ikke sende email',  
+            visibilityTime: 5000,
+            position: 'top',
+          });
+          throw error;
+      });
 };
