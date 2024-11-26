41649297c1c17d508fa1b5c0cb037c56fbc77371
naja moe <najamoe@outlook.dk>
2024-11-26 16:15:01 +0100

    fixed doc setup

--- app/firebase/firestoreService.js
+++ app/firebase/firestoreService.js
@@ -1,13 +1,15 @@
-import { firestore } from "./FirebaseConfig";
+import { doc, setDoc } from "firebase/firestore"; // Modular Firestore imports
+import firebaseConfig from "./FirebaseConfig";
+
+const { db } = firebaseConfig; // Get Firestore instance
 
 export const addUserToFirestore = async (uid, userData) => {
   try {
-    await firestore.collection("users").doc(uid).set(userData);
+    const userRef = doc(db, "users", uid); // Use doc() to reference the document
+    await setDoc(userRef, userData); // Use setDoc to write data
     console.log("User added to Firestore:", userData);
   } catch (error) {
     console.error("Error adding user to Firestore:", error.message);
     throw error;
   }
 };
-
-
