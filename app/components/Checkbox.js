c9818cc7940b9199aed23057d2b5d317344e6c9e
naja moe <najamoe@outlook.dk>
2024-12-16 10:33:48 +0100

    checkbox component created

---
+++ app/components/Checkbox.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const Checkbox = ({ options, checkedValues, onChange, style }) => {
+  return (
+    <View style={[styles.container, style]}>
+      {options.map((option) => {
+        const active = checkedValues.includes(option.value);
+
+        return (
+          <TouchableOpacity
+            key={option.value}
+            style={active ? [styles.checkbox, styles.activeCheckbox] : styles.checkbox}
+            onPress={() => {
+              const updatedCheckedValues = active
+                ? checkedValues.filter((value) => value !== option.value)
+                : [...checkedValues, option.value];
+              onChange(updatedCheckedValues);
+            }}
+          >
+            <MaterialIcons
+              name={active ? "check-box" : "check-box-outline-blank"}
+              size={24}
+              color={active ? "#06b6d4" : "#64748b"}
+            />
+            <Text style={active ? [styles.text, styles.activeText] : styles.text}>
+              {option.label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+  },
+  checkbox: {
+    height: 60,
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10,
+    backgroundColor: "#f3f4f6",
+    paddingHorizontal: 15,
+    borderRadius: 10,
+  },
+  activeCheckbox: {
+    backgroundColor: "#06b6d4" + "11",
+    borderColor: "blue",
+  },
+  text: {
+    fontSize: 16,
+    marginLeft: 15,
+    color: "#6b7280",
+  },
+  activeText: {
+    color: "#374151",
+  },
+});
+
+export default Checkbox;
