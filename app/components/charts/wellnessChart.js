295fee824c68d50a76d29575efc4a7f9c18be7ec
naja moe <najamoe@outlook.dk>
2024-12-20 11:18:45 +0100

    started work on waterChart

--- app/components/charts/wellnessChart.js
+++ app/components/charts/wellnessChart.js
@@ -3,12 +3,35 @@ import React from 'react'
 
 const wellnessChart = () => {
   return (
-    <View>
-      <Text>wellnessChart</Text>
+    <View style={styles.chartContainer}>
+      <Text style={styles.title}>Humør</Text>
+      
     </View>
   )
 }
 
 export default wellnessChart
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  chartContainer: {  
+    marginTop: 200,
+    backgroundColor: "white",
+    borderRadius: 10,
+    width: "96%",
+    height: 300,
+    marginVertical: 10,
+    marginLeft: "3%", 
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+})
