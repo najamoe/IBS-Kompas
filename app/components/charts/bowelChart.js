b02382a395ca08ba6a9ad2e02dc8f44f6cdb0854
naja moe <najamoe@outlook.dk>
2024-12-27 11:50:38 +0100

    added charts

---
+++ app/components/charts/bowelChart.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BowelChart = () => {
+  return (
+    <View>
+      <Text>bowelChart</Text>
+    </View>
+  )
+}
+
+export default BowelChart
+
+const styles = StyleSheet.create({})
