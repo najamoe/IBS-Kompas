4f7da09f1c2ff7ee521dfc4af589de5f4445c04c
Najamoe <naja0718@stud.kea.dk>
2024-12-28 11:22:12 +0100

    added loading spinner to custom buttons

--- app/components/CustomButton.js
+++ app/components/CustomButton.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'; 
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-const CustomButton = ({ title, handlePress, customStyles, textStyles, isLoading, iconName }) => {
+const CustomButton = ({
+  title,
+  handlePress,
+  customStyles,
+  textStyles,
+  isLoading,
+  iconName,
+  loadingText,
+}) => {
   return (
     <TouchableOpacity
-      style={[styles.button, customStyles, isLoading && styles.loading]} 
+      style={[styles.button, customStyles, isLoading && styles.loading]}
       onPress={handlePress}
       activeOpacity={0.7}
-      disabled={isLoading} 
+      disabled={isLoading}
     >
       <View style={styles.buttonContent}>
         {iconName && (
-          <Ionicons name={iconName} size={16} color="white" style={styles.icon} /> // Icon with size and color
+          <Ionicons
+            name={iconName}
+            size={16}
+            color="white"
+            style={styles.icon}
+          /> // Icon with size and color
         )}
-        <Text style={[styles.buttonText, textStyles]}>{title}</Text>
-        {isLoading ? (loadingText || 'Loading...') : title}
+        <Text style={[styles.buttonText, textStyles]}>
+          {isLoading ? loadingText || "Loading..." : title}
+        </Text>
       </View>
     </TouchableOpacity>
   );
@@ -25,28 +39,28 @@ export default CustomButton;
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: '#86c5d8',
+    backgroundColor: "#86c5d8",
     paddingVertical: 10,
     paddingHorizontal: 5,
     borderRadius: 40,
     marginTop: 20,
     marginBottom: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     width: 150,
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginLeft: 8, // Space between icon and text
   },
   loading: {
-    opacity: 0.5, 
+    opacity: 0.5,
   },
   buttonContent: {
-    flexDirection: 'row', // Layout for icon and text (horizontal)
-    alignItems: 'center', // Vertically center both
+    flexDirection: "row", // Layout for icon and text (horizontal)
+    alignItems: "center", // Vertically center both
   },
   icon: {
     marginRight: 8, // Space between icon and text
