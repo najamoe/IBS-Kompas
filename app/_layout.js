5f60be13696dcf6eaf72497578694216f8fc1439
Najamoe <naja0718@stud.kea.dk>
2024-12-31 10:53:27 +0100

    added toast

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Toast from 'react-native-toast-message';
 import { Slot, Stack } from 'expo-router';
 
 const _layout = () => {
   return (
-    <Stack screenOptions={{headerShown: false}}>
-      <Stack.Screen name="index" options={{headerShown : false}} />
+    <View style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
       
-    </Stack>
-  )
+      </Stack>
+
+      
+      <Toast />
+    </View>
+  );
 }
 
 export default _layout
