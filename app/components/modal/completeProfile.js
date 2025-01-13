a28256aae61e47f99074b6a50338e21db6f5bdb8
naja moe <najamoe@outlook.dk>
2025-01-13 10:41:39 +0100

    Added completeProfilemodal

---
+++ app/components/modal/completeProfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const completeProfile = () => {
+  return (
+    <View>
+      <Text>completeProfile</Text>
+    </View>
+  )
+}
+
+export default completeProfile
+
+const styles = StyleSheet.create({})
