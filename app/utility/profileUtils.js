2d34d27418fad64b1abc771e994c987e138e226f
naja moe <najamoe@outlook.dk>
2025-01-13 12:01:43 +0100

    seperated completed profile and update logic to utils folder

---
+++ app/utility/profileUtils.js
@@ -0,0 +1,39 @@
+import { updateUserDetails } from "../services/firebase/userService";
+
+export const checkCompletedProfile = async (userData, setUserData) => {
+  const { name, birthday, gender, ibsType, waterGoal } = userData || {};
+
+  // Log userData to check values
+  console.log("User Data:", userData);
+
+  // Validate each field and check if all fields are filled
+  const isCompleted =
+    name &&
+    birthday &&
+    gender &&
+    ibsType &&
+    waterGoal &&
+    name.trim() !== "" &&
+    birthday.trim() !== "" &&
+    gender.trim() !== "" &&
+    ibsType.trim() !== "" &&
+    waterGoal.trim() !== "";
+
+  // Ensure profileCompleted is a Boolean value
+  const updatedProfileCompleted = Boolean(isCompleted);
+
+  if (updatedProfileCompleted !== userData.profileCompleted) {
+    // Update Firestore with the correct profileCompleted status
+    try {
+      await updateUserDetails(userData.uid, {
+        profileCompleted: updatedProfileCompleted,
+      });
+      setUserData((prevData) => ({
+        ...prevData,
+        profileCompleted: updatedProfileCompleted, // Update the local state
+      }));
+    } catch (error) {
+      console.error("Error updating completedProfile:", error);
+    }
+  }
+};
